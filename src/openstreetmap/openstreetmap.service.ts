8d2374289e7da7ddaf020ad6b58bcabc1e88d5dc
Trevor Newell <trevor@mantl.com>
2022-02-03 16:36:47 -0500

    Provide example OSM implementation

--- src/openstreetmap/openstreetmap.service.ts
+++ src/openstreetmap/openstreetmap.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common'
+import axios from 'axios'
 
 @Injectable()
 export class OpenstreetmapService {
+  private httpClient
+
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https://nominatim.openstreetmap.org',
+    })
+  }
+
   async getCoordinates(location: string) {
-    throw Error('not implemented')
+    const response = await this.httpClient.get('/search', {
+      params: { q: location, format: 'json' },
+    })
+
+    if (!response.data) {
+      return undefined
+    }
+    return {
+      latitude: response.data[0]?.lat,
+      longitude: response.data[0]?.lon,
+    }
   }
 }
